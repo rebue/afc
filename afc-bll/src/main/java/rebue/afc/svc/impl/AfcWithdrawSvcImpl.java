d6859a3cf408111e4a12e8d6071cd4cb8ad136f8
lbl <lbl@wboly-lbl>
2018-10-22 17:05:30 +0800

    修改申请提现和确认提现

--- afc-bll/src/main/java/rebue/afc/svc/impl/AfcWithdrawSvcImpl.java
+++ afc-bll/src/main/java/rebue/afc/svc/impl/AfcWithdrawSvcImpl.java
@@ -1,7 +1,6 @@
 package rebue.afc.svc.impl;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -33,6 +32,7 @@ import rebue.afc.svc.AfcFlowSvc;
 import rebue.afc.svc.AfcTradeSvc;
 import rebue.afc.svc.AfcWithdrawAccountSvc;
 import rebue.afc.svc.AfcWithdrawSvc;
+import rebue.afc.to.ApplyWithdrawTo;
 import rebue.afc.withdraw.dic.WithdrawApplyResultDic;
 import rebue.afc.withdraw.dic.WithdrawCancelResultDic;
 import rebue.afc.withdraw.dic.WithdrawDealResultDic;
@@ -42,13 +42,17 @@ import rebue.afc.withdraw.ro.WithdrawApplyRo;
 import rebue.afc.withdraw.ro.WithdrawCancelRo;
 import rebue.afc.withdraw.ro.WithdrawDealRo;
 import rebue.afc.withdraw.ro.WithdrawOkRo;
-import rebue.afc.withdraw.to.WithdrawApplyTo;
 import rebue.afc.withdraw.to.WithdrawCancelTo;
 import rebue.afc.withdraw.to.WithdrawDealTo;
 import rebue.afc.withdraw.to.WithdrawOkTo;
+import rebue.rna.mo.RnaRealnameMo;
+import rebue.rna.svr.feign.RnaSvc;
+import rebue.robotech.ro.Ro;
 import rebue.robotech.svc.impl.MybatisBaseSvcImpl;
 import rebue.suc.mo.SucUserMo;
 import rebue.suc.svr.feign.SucUserSvc;
+import rebue.wheel.IdCardValidator;
+import rebue.wheel.RegexUtils;
 
 /**
  * 提现信息
@@ -99,131 +103,177 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 
 	@Resource
 	private Mapper dozerMapper;
-	
+
 	@Resource
 	private AfcWithdrawSvc afcWithdrawSvc;
-	
+
 	@Resource
 	private AfcPlatformTradeSvc afcPlatformTradeSvc;
+
+	@Resource
+	private SucUserSvc sucUserSvc;
 	
 	@Resource
-    private SucUserSvc sucUserSvc;
+	private RnaSvc rnaSvc;
 
 	/**
 	 * 提现申请（ 余额-，提现中+ ）
+	 * 流程：
+	 * 	1、判断参数
+	 * 	2、验证身份证号
+	 * 	3、查询申请人用户信息
+	 * 	4、查询申请人是否已被锁定
+	 * 	5、查询申请人账户余额，并判断是否足以扣减提现金额 + 提现服务费
+	 * 	6、添加提现申请信息
+	 * 	7、添加实名认证信息
+	 * 	8、添加申请人账户申请提现交易信息
 	 */
 	@Override
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
-	public WithdrawApplyRo apply(WithdrawApplyTo to) {
-		if (to.getUserId() == null || to.getTradeAmount() == null || to.getOpId() == null
-				|| StringUtils.isAnyBlank(to.getMac(), to.getIp())) {
-			_log.warn("没有填写申请提现账户的用户ID/申请提现的金额/申请人的用户ID/MAC/IP: {}", to);
-			WithdrawApplyRo ro = new WithdrawApplyRo();
+	public WithdrawApplyRo apply(ApplyWithdrawTo to) {
+		_log.info("申请提现的参数为：{}", to);
+		WithdrawApplyRo ro = new WithdrawApplyRo();
+		if (StringUtils.isAnyBlank(to.getBankAccountName(), to.getBankAccountNo(), to.getContactTel(),
+				to.getIp()) || to.getApplicantId() == null || to.getWithdrawAmount() == null || to.getWithdrawType() == null) {
+			_log.error("申请提现出现参数错误");
 			ro.setResult(WithdrawApplyResultDic.PARAM_ERROR);
+			ro.setMsg("参数错误");
 			return ro;
 		}
-		SucUserMo opUserMo = userSvc.getById(to.getOpId());
-		if (opUserMo == null) {
-			_log.error("提现申请发现没有此申请人: " + to.getOpId());
-			WithdrawApplyRo ro = new WithdrawApplyRo();
+		
+		if (!RegexUtils.matchMobile(to.getContactTel())) {
+			ro.setResult(WithdrawApplyResultDic.PARAM_ERROR);
+			ro.setMsg("手机号码格式不正确");
+			return ro;
+		}
+		
+		// 查询申请人基本信息
+		_log.info("申请提现查询用户信息的参数为：{}", to.getApplicantId());
+		SucUserMo sucUserMo = sucUserSvc.getById(to.getApplicantId());
+		_log.info("申请提现查询用户信息的返回值为：{}", sucUserMo);
+		if (sucUserMo == null) {
+			_log.error("申请提现查询用户信息时发现该用户不存在，申请人id为：{}", to.getApplicantId());
 			ro.setResult(WithdrawApplyResultDic.NOT_FOUND_OP);
+			ro.setMsg("非法用户，请重新登录后再试。。。");
 			return ro;
 		}
-		if (opUserMo.getIsLock()) {
-			_log.error("提现申请发现申请人已被锁定: " + opUserMo);
-			WithdrawApplyRo ro = new WithdrawApplyRo();
+		
+		if (sucUserMo.getIsLock()) {
+			_log.error("申请提现时发现该申请人已被锁定，申请人id为：{}", to.getApplicantId());
 			ro.setResult(WithdrawApplyResultDic.OP_LOCKED);
+			ro.setMsg("您已被锁定，请解锁后再重试。。。");
 			return ro;
 		}
-		SucUserMo withdrawUserMo = userSvc.getById(to.getUserId());
-		if (withdrawUserMo == null) {
-			_log.error("没有发现提现的用户: " + to.getUserId());
-			WithdrawApplyRo ro = new WithdrawApplyRo();
-			ro.setResult(WithdrawApplyResultDic.NOT_FOUND_WITHDRAW_USER);
+		
+		// 查询申请人账户信息
+		_log.info("申请提现查询申请人账户信息的参数为：{}", to.getApplicantId());
+		AfcAccountMo accountMo = accountSvc.getById(to.getApplicantId());
+		_log.info("申请提现查询申请人账户信息的返回值为：{}", accountMo);
+		if (accountMo == null) {
+			_log.error("申请提现查询申请人账户信息时发现该申请人不存在提现账户，申请人id为：{}", to.getApplicantId());
+			ro.setResult(WithdrawApplyResultDic.NOT_FOUND_OP);
+			ro.setMsg("非法用户，请重新登录后再试。。。");
 			return ro;
 		}
-		if (withdrawUserMo.getIsLock()) {
-			_log.error("提现的用户已经被锁定: " + withdrawUserMo);
-			WithdrawApplyRo ro = new WithdrawApplyRo();
-			ro.setResult(WithdrawApplyResultDic.WITHDRAW_USER_LOCKED);
-			return ro;
-		}
-		_log.info("申请提现查询用户提现次数和手续费的参数为：{}", to.getUserId());
+		
+		// 查询本月已提现次数
 		AfcWithdrawMo afcWithdrawMo = new AfcWithdrawMo();
-		afcWithdrawMo.setAccountId(to.getUserId());
-		// 查询用户提现次数和手续费
+		afcWithdrawMo.setAccountId(to.getApplicantId());
+		_log.info("申请提现查询本月已提现次数的参数为：{}", afcWithdrawMo);
 		WithdrawNumberForMonthRo withdrawNumberForMonthRo = afcWithdrawSvc.getWithdrawNumberForMonth(afcWithdrawMo);
-		_log.info("申请提现查询用户提现次数和手续费的返回值为：{}", withdrawNumberForMonthRo);
-		// 用户的提现账户
-		AfcWithdrawAccountMo withdrawAccountMo;
-		// 如果没有传withdrawAccountId(提现账户参数)，获取用户的默认提现账户
-		if (to.getWithdrawAccountId() == null) {
-			withdrawAccountMo = getDefaultWithdrawAccount(to.getUserId());
-		} else {
-			withdrawAccountMo = withdrawAccountSvc.getById(to.getWithdrawAccountId());
-		}
-		if (withdrawAccountMo == null) {
-			_log.error("没有发现用户的提现账户: userId-" + to.getUserId() + ", withdrawAccountId-" + to.getWithdrawAccountId());
-			WithdrawApplyRo ro = new WithdrawApplyRo();
-			ro.setResult(WithdrawApplyResultDic.NOT_FOUND_WITHDRAW_ACCOUNT);
-			return ro;
-		}
-		// 查询旧账户信息
-		AfcAccountMo oldAccountMo = accountSvc.getById(to.getUserId());
-		int compareTo = oldAccountMo.getBalance().compareTo(withdrawNumberForMonthRo.getSeviceCharge().add(new BigDecimal(to.getTradeAmount()), new MathContext(4)));
-		_log.info("申请提现账号余额与提现金额+服务费对比的结果为：{}", compareTo);
-		// 判断提现账户的余额是否足够（小于提现金额）
-		if (compareTo < 0) {
-			_log.error("提现用户的余额不足: " + oldAccountMo);
-			WithdrawApplyRo ro = new WithdrawApplyRo();
+		_log.info("申请提现查询本月已提现次数的返回值为：{}", withdrawNumberForMonthRo);
+		// 申请人账户余额
+		BigDecimal balance = accountMo.getBalance().setScale(4, BigDecimal.ROUND_HALF_UP);
+		if (balance.compareTo(to.getWithdrawAmount().add(withdrawNumberForMonthRo.getSeviceCharge()).setScale(4, BigDecimal.ROUND_HALF_UP)) < 0) {
+			_log.info("申请提现申请账户余额对比提现金额+提现服务费时出现账户余额不足以扣减，申请人id为：{}", to.getApplicantId());
 			ro.setResult(WithdrawApplyResultDic.NOT_ENOUGH_MONEY);
+			ro.setMsg("您的余额不足");
 			return ro;
 		}
-		Date now = new Date();
-		// 得到交易金额
-		BigDecimal tradeAmount = new BigDecimal(to.getTradeAmount().toString());
-		// 添加提现申请
+		
+		// 添加申请提现信息
 		AfcWithdrawMo withdrawMo = new AfcWithdrawMo();
-		withdrawMo.setAccountId(oldAccountMo.getId());
-		withdrawMo.setWithdrawState((byte) WithdrawStateDic.PROCESSING.getCode());
-		// 申请提现的单号
+		withdrawMo.setAccountId(to.getApplicantId());
 		withdrawMo.setOrderId(to.getOrderId());
+		withdrawMo.setWithdrawState((byte) 2);
 		withdrawMo.setTradeTitle(to.getTradeTitle());
 		withdrawMo.setTradeDetail(to.getTradeDetail());
-		// 提现金额
-		withdrawMo.setAmount(tradeAmount);
-		// 提现服务费 TODO 添加查询提服务费的接口
-		withdrawMo.setSeviceCharge(BigDecimal.ZERO);
-		// 实际到账金额(提现金额-提现服务费)
-		withdrawMo.setRealAmount(tradeAmount.subtract(withdrawMo.getSeviceCharge()));
-		withdrawMo.setApplicantId(to.getOpId());
-		withdrawMo.setApplyTime(now);
-		withdrawMo.setApplicantMac(to.getMac());
+		withdrawMo.setAmount(to.getWithdrawAmount());
+		withdrawMo.setRealAmount(to.getWithdrawAmount());
+		withdrawMo.setSeviceCharge(new BigDecimal("0"));
+		withdrawMo.setAccountId(to.getApplicantId());
+		withdrawMo.setApplicantId(to.getApplicantId());
+		withdrawMo.setApplyTime(new Date());
+		withdrawMo.setApplicantMac("不再获取mac地址");
 		withdrawMo.setApplicantIp(to.getIp());
-		// 记录当前提现账户详细信息
-		withdrawMo.setWithdrawType(withdrawAccountMo.getWithdrawType());
-		withdrawMo.setContactTel(withdrawAccountMo.getContactTel());
-		withdrawMo.setBankAccountNo(withdrawAccountMo.getBankAccountNo());
-		withdrawMo.setBankAccountName(withdrawAccountMo.getBankAccountName());
-		withdrawMo.setOpenAccountBank(withdrawAccountMo.getOpenAccountBank());
-		add(withdrawMo);
-		// 添加一笔交易
-		AfcTradeMo tradeMo = dozerMapper.map(to, AfcTradeMo.class);
-		tradeMo.setAccountId(to.getUserId());
+		withdrawMo.setWithdrawType(to.getWithdrawType());
+		withdrawMo.setContactTel(to.getContactTel());
+		withdrawMo.setBankAccountNo(to.getBankAccountNo());
+		withdrawMo.setBankAccountName(to.getBankAccountName());
+		withdrawMo.setOpenAccountBank(to.getWithdrawType() == 1 ? to.getOpenAccountBank() : "支付宝网银");
+		_log.info("申请提现添加提现信息的参数为：{}", withdrawMo);
+		int addResult = afcWithdrawSvc.add(withdrawMo);
+		_log.info("申请提现添加提现信息的返回值为：{}", addResult);
+		if (addResult != 1) {
+			_log.info("申请提现添加提现信息时出现错误，申请人id为：{}", to.getApplicantId());
+			ro.setResult(WithdrawApplyResultDic.ERROR);
+			ro.setMsg("提现出错");
+			return ro;
+		}
+		
+		// 添加一笔账户交易
+		AfcTradeMo tradeMo = new AfcTradeMo();
+		tradeMo.setAccountId(to.getApplicantId());
 		tradeMo.setTradeType((byte) TradeTypeDic.WITHDRAW_APPLY.getCode());
-		tradeMo.setTradeTime(now);
+		tradeMo.setTradeTime(new Date());
+		tradeMo.setTradeAmount(to.getWithdrawAmount());
+		// 交易标题
+		tradeMo.setTradeTitle(to.getTradeTitle());
+		// 交易详情
+		tradeMo.setTradeDetail(to.getTradeDetail());
+		// 申请提现的单号
+		tradeMo.setOrderId(to.getOrderId());
+		tradeMo.setOpId(to.getApplicantId());
+		tradeMo.setMac("不再获取mac地址");
+		tradeMo.setIp(to.getIp());
+		_log.info("确认提现添加提现服务费账户交易信息的参数为：{}", tradeMo);
 		tradeSvc.addTrade(tradeMo);
-		// 返回成功
-		_log.info("申请提现提交成功: {}", to);
-		WithdrawApplyRo ro = new WithdrawApplyRo();
+		
+		RnaRealnameMo rnaRealnameMo = rnaSvc.getById(to.getApplicantId());
+		if (rnaRealnameMo == null) {
+			if (to.getIdCard() == null || to.getIdCard().equals("") || to.getIdCard().equals("null")) {
+				throw new RuntimeException("身份证号不正确");
+			}
+			// 验证身份证号
+			if (!IdCardValidator.validate(to.getIdCard())) {
+				_log.error("申请提现验证身份证号时出现身份证号格式不正确，申请人id为：{}", to.getApplicantId());
+				throw new RuntimeException("身份证号格式不正确");
+			}
+			
+			RnaRealnameMo realnameMo = new RnaRealnameMo();
+			realnameMo.setId(to.getApplicantId());
+			realnameMo.setRealName(to.getBankAccountName());
+			realnameMo.setIdCard(to.getIdCard());
+			realnameMo.setIsCorrect(false);
+			_log.info("申请提现添加实名认证信息的参数为：{}", realnameMo);
+			Ro addRnaRealNameRo = rnaSvc.add(realnameMo);
+			_log.info("申请提现添加实名认证信息的返回值为：{}", addRnaRealNameRo);
+			if (addRnaRealNameRo.getResult().getCode() != 1) {
+				_log.error("申请提现添加实名认证信息出错，申请人id为：{}", to.getApplicantId());
+				throw new RuntimeException("添加出错");
+			}
+		}
+		
+		_log.info("申请提现成功，申请人id为：{}", to.getApplicantId());
 		ro.setResult(WithdrawApplyResultDic.SUCCESS);
-		ro.setId(withdrawMo.getId());
+		ro.setMsg("申请成功");
 		return ro;
 	}
 
 	/**
 	 * 获取用户的默认提现账户
 	 */
+	@SuppressWarnings("unused")
 	private AfcWithdrawAccountMo getDefaultWithdrawAccount(Long userId) {
 		AfcWithdrawAccountMo withdrawAccountMo;
 		List<AfcWithdrawAccountMo> withdrawAccountMos = withdrawAccountSvc.listByUserId(userId);
@@ -292,6 +342,20 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 
 	/**
 	 * 确认提现成功（手动）（ 提现中- ）
+	 * 流程：
+	 * 	1、判断参数是否正确
+	 * 	2、查询审核人是否存在
+	 * 	3、判断审核人是否已被锁定
+	 * 	4、查询是否有该提现记录
+	 * 	5、查询该提现申请是否已被处理
+	 * 	6、查询提现次数和服务费（已成功）
+	 * 	7、查询提现用户账户余额是否足够扣减服务费
+	 * 	8、查询是否已有提现账号，如果没有则添加
+	 * 	9、修改实名认证信息
+	 * 	10、修改提现信息
+	 * 	11、添加提现账号提现交易信息
+	 * 	12、添加提现账号提现服务费交易信息
+	 * 	13、添加平台提现手续费交易信息
 	 */
 	@Override
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
@@ -333,18 +397,48 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 		}
 		AfcWithdrawMo afcWithdrawMo = new AfcWithdrawMo();
 		afcWithdrawMo.setAccountId(to.getAccountId());
-		afcWithdrawMo.setWithdrawState((byte)3);
+		afcWithdrawMo.setWithdrawState((byte) 3);
 		_log.info("确认提现成功查询提现次数和服务费的参数为：{}", afcWithdrawMo);
 		WithdrawNumberForMonthRo withdrawNumberForMonthRo = afcWithdrawSvc.getWithdrawNumberForMonth(afcWithdrawMo);
 		_log.info("确认提现成功查询提现次数和服务费的返回值为：{}", withdrawNumberForMonthRo);
-		
+
 		_log.info("确认提现成功查询账户信息的参数为：{}", to.getAccountId());
 		AfcAccountMo accountMo = accountSvc.getById(to.getAccountId());
 		_log.info("确认提现成功查询账户信息的返回值为：{}", accountMo);
 		if (accountMo.getBalance().compareTo(withdrawNumberForMonthRo.getSeviceCharge()) < 0) {
+			_log.error("确认提现成功查询账户余额时发现，余额不足以抵扣服务费，提现人id为：{}", to.getAccountId());
 			ro.setResult(WithdrawOkResultDic.SUBTRACT_SERVICE_CHARGE_ERROR);
 			ro.setMsg("扣减服务费失败");
 			return ro;
+		}
+
+		AfcWithdrawAccountMo withdrawAccountMo = new AfcWithdrawAccountMo();
+		withdrawAccountMo.setAccountId(withdrawMo.getAccountId());
+		_log.info("确认提现成功查询提现账号信息的参数为：{}", withdrawAccountMo);
+		AfcWithdrawAccountMo afcWithdrawAccountMo = withdrawAccountSvc.getOne(withdrawAccountMo);
+		_log.info("确认提现成功查询提现账号信息的返回值为：{}", afcWithdrawAccountMo);
+		if (afcWithdrawAccountMo == null) {
+			afcWithdrawAccountMo = new AfcWithdrawAccountMo();
+			afcWithdrawAccountMo.setIsDef(true);
+			afcWithdrawAccountMo.setAccountId(to.getAccountId());
+			afcWithdrawAccountMo.setWithdrawType(withdrawMo.getWithdrawType());
+			afcWithdrawAccountMo.setContactTel(withdrawMo.getContactTel());
+			afcWithdrawAccountMo.setBankAccountNo(withdrawMo.getBankAccountNo());
+			afcWithdrawAccountMo.setBankAccountName(withdrawMo.getBankAccountName());
+			afcWithdrawAccountMo.setOpenAccountBank(withdrawMo.getOpenAccountBank());
+			afcWithdrawAccountMo.setOpId(to.getOpId());
+			afcWithdrawAccountMo.setMac("不再获取mac地址");
+			afcWithdrawAccountMo.setIp(to.getIp());
+			afcWithdrawAccountMo.setModifiedTimestamp(System.currentTimeMillis());
+			_log.info("确认提现添加提现账户信息的参数为：{}", afcWithdrawAccountMo);
+			int addWithdrawAccountResult = withdrawAccountSvc.add(afcWithdrawAccountMo);
+			_log.info("确认提现添加提现账户信息的返回值为：{}", addWithdrawAccountResult);
+			if (addWithdrawAccountResult != 1) {
+				_log.error("确认提现添加提现账号信息出错，提现用户id为：{}", to.getAccountId());
+				ro.setResult(WithdrawOkResultDic.ADD_WITHDRAW_ACCOUNT_ERROR);
+				ro.setMsg("添加提现账户出错");
+				return ro;
+			}
 		}
 		
 		// 交易金额
@@ -366,9 +460,8 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 		int modifyResult = afcWithdrawSvc.modify(modifyMo);
 		_log.info("确认提现成功修改提现信息的返回值为：{}", modifyResult);
 		if (modifyResult != 1) {
-			ro.setResult(WithdrawOkResultDic.ERROR);
-			ro.setMsg("操作失败");
-			return ro;
+			_log.error("确认提现成功修改提现信息出错，提现用户id为：{}", to.getAccountId());
+			throw new RuntimeException("操作失败");
 		}
 		// 添加一笔交易
 		AfcTradeMo tradeMo = new AfcTradeMo();
@@ -387,7 +480,7 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 		tradeMo.setIp(to.getIp());
 		_log.info("确认提现添加提现账户交易信息的参数为：{}", tradeMo);
 		tradeSvc.addTrade(tradeMo);
-		
+
 		// 添加一笔交易
 		tradeMo = new AfcTradeMo();
 		tradeMo.setAccountId(withdrawMo.getAccountId());
@@ -405,7 +498,7 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 		tradeMo.setIp(to.getIp());
 		_log.info("确认提现添加提现服务费账户交易信息的参数为：{}", tradeMo);
 		tradeSvc.addTrade(tradeMo);
-		
+
 		AfcPlatformTradeMo platformTradeMo = new AfcPlatformTradeMo();
 		platformTradeMo.setPlatformTradeType((byte) PlatformTradeTypeDic.WITHDRAW_SEVICE_CHARGE.getCode());
 		platformTradeMo.setOrderId(withdrawMo.getOrderId());
@@ -414,6 +507,17 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 		platformTradeMo.setModifiedTimestamp(System.currentTimeMillis());
 		_log.info("确认提现添加平台账户交易信息的参数为：{}", tradeMo);
 		afcPlatformTradeSvc.addTrade(platformTradeMo);
+		
+		RnaRealnameMo realnameMo = new RnaRealnameMo();
+		realnameMo.setId(to.getAccountId());
+		realnameMo.setIsCorrect(true);
+		_log.info("确认提现修改实名认证信息的参数为：{}", realnameMo);
+		Ro modifyRnaResult = rnaSvc.modify(realnameMo);
+		_log.info("确认提现修改实名认证信息的返回值为：{}", modifyRnaResult);
+		if (modifyRnaResult.getResult().getCode() != 1) {
+			throw new RuntimeException("操作出错");
+		}
+		
 		// 返回成功
 		_log.info("确认提现成功（手动）成功: {}", to);
 		ro.setResult(WithdrawOkResultDic.SUCCESS);
@@ -423,6 +527,14 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 
 	/**
 	 * 作废提现（ 余额+，提现中- ）
+	 * 流程：
+	 * 	1、判断参数
+	 * 	2、查询操作人信息
+	 * 	3、查询操作人是否已被锁定
+	 * 	4、查询是否有该申请信息
+	 * 	5、查询该申请是否已被处理
+	 * 	6、修改提现信息
+	 * 	7、添加申请人账户作废提现交易信息
 	 */
 	@Override
 	public WithdrawCancelRo cancel(WithdrawCancelTo to) {
@@ -474,7 +586,15 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 		modifyMo.setWithdrawState((byte) WithdrawStateDic.CANCEL.getCode());
 		// 作废原因
 		modifyMo.setCancelReason(to.getReason());
-		modify(modifyMo);
+		_log.error("作废提现修改提现信息的参数为：{}", modifyMo);
+		int modifyResult = modify(modifyMo);
+		_log.info("作废提现修改提现信息的返回值为：{}", modifyResult);
+		if (modifyResult != 1) {
+			WithdrawCancelRo ro = new WithdrawCancelRo();
+			ro.setResult(WithdrawCancelResultDic.ERROR);
+			ro.setMsg("操作失败");
+			return ro;
+		}
 		// 添加一笔交易
 		AfcTradeMo tradeMo = new AfcTradeMo();
 		tradeMo.setAccountId(withdrawMo.getAccountId());
@@ -519,9 +639,10 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 		numberForMonthRo.setSeviceCharge(new BigDecimal(pow));
 		return numberForMonthRo;
 	}
-	
+
 	/**
 	 * 重写查询提现信息
+	 * 
 	 * @param mo
 	 * @param pageNum
 	 * @param pageSize
@@ -532,19 +653,20 @@ public class AfcWithdrawSvcImpl extends MybatisBaseSvcImpl<AfcWithdrawMo, java.l
 	@SuppressWarnings("unchecked")
 	public PageInfo<AfcWithdrawRo> lisrEx(AfcWithdrawMo mo, int pageNum, int pageSize, String orderBy) {
 		PageInfo<AfcWithdrawRo> doSelectPageInfoEx = new PageInfo<AfcWithdrawRo>();
-    	List<AfcWithdrawRo> listEx = new ArrayList<AfcWithdrawRo>();
-    	PageInfo<AfcWithdrawMo> doSelectPageInfo = PageHelper.startPage(pageNum, pageSize, orderBy).doSelectPageInfo(() -> _mapper.selectSelective(mo));
-    	for (AfcWithdrawMo afcWithdrawMo : doSelectPageInfo.getList()) {
-    		AfcWithdrawRo afcWithdrawRo = dozerMapper.map(afcWithdrawMo, AfcWithdrawRo.class);
-    		_log.info("查询申请提现账号记录查询用户信息的参数为：{}", afcWithdrawMo.getApplicantId());
-    		SucUserMo sucUserMo = sucUserSvc.getById(afcWithdrawMo.getApplicantId());
-    		_log.info("查询申请提现账号记录查询用户信息的返回值为：{}", sucUserMo);
-    		afcWithdrawRo.setApplicantName(sucUserMo.getWxNickname());
-    		listEx.add(afcWithdrawRo);
+		List<AfcWithdrawRo> listEx = new ArrayList<AfcWithdrawRo>();
+		PageInfo<AfcWithdrawMo> doSelectPageInfo = PageHelper.startPage(pageNum, pageSize, orderBy)
+				.doSelectPageInfo(() -> _mapper.selectSelective(mo));
+		for (AfcWithdrawMo afcWithdrawMo : doSelectPageInfo.getList()) {
+			AfcWithdrawRo afcWithdrawRo = dozerMapper.map(afcWithdrawMo, AfcWithdrawRo.class);
+			_log.info("查询申请提现账号记录查询用户信息的参数为：{}", afcWithdrawMo.getApplicantId());
+			SucUserMo sucUserMo = sucUserSvc.getById(afcWithdrawMo.getApplicantId());
+			_log.info("查询申请提现账号记录查询用户信息的返回值为：{}", sucUserMo);
+			afcWithdrawRo.setApplicantName(sucUserMo.getWxNickname());
+			listEx.add(afcWithdrawRo);
 		}
-    	doSelectPageInfoEx = dozerMapper.map(doSelectPageInfo, PageInfo.class);
-    	doSelectPageInfoEx.setList(listEx);
-    	_log.info("查询申请提现账号记录的返回值为：{}", doSelectPageInfoEx);
-    	return doSelectPageInfoEx;
+		doSelectPageInfoEx = dozerMapper.map(doSelectPageInfo, PageInfo.class);
+		doSelectPageInfoEx.setList(listEx);
+		_log.info("查询申请提现账号记录的返回值为：{}", doSelectPageInfoEx);
+		return doSelectPageInfoEx;
 	}
 }
