2ff8abe8f3c6b9ae70c44d6eb3b74b14a3577863
zbz <nnzbz@qq.com>
2018-11-22 15:42:56 +0800

    * 优化代码

--- afc-svr/src/main/java/rebue/afc/ctrl/AfcChargeCtrl.java
+++ afc-svr/src/main/java/rebue/afc/ctrl/AfcChargeCtrl.java
@@ -12,13 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.pagehelper.PageInfo;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import rebue.afc.dic.ChargeResultDic;
 import rebue.afc.dic.ChargeTypeDic;
-import rebue.afc.dic.TradeTypeDic;
 import rebue.afc.ro.ChargeRo;
 import rebue.afc.svc.AfcChargeSvc;
 import rebue.afc.to.ChargeTo;
@@ -29,58 +25,43 @@ import rebue.wheel.turing.JwtUtils;
 @Api(tags = "余额或返现金充值")
 @RestController
 public class AfcChargeCtrl {
-	
-	private final static Logger _log = LoggerFactory.getLogger(AfcDepositCtrl.class);
 
-	/**
-	 * 是否测试模式（测试模式下不用从Cookie中获取用户ID）
-	 */
-	@Value("${debug}")
-	private Boolean isDebug;
-	
-	 /**
+    private final static Logger _log = LoggerFactory.getLogger(AfcDepositCtrl.class);
+
+    /**
+     * 是否测试模式（测试模式下不用从Cookie中获取用户ID）
+     */
+    @Value("${debug:false}")
+    private Boolean             isDebug;
+
+    /**
      * 前面经过的代理
      */
-    @Value("${suc.passProxy:noproxy}")
+    @Value("${afc.passProxy:noproxy}")
     private String              passProxy;
-    
+
     @Resource
-    private AfcChargeSvc       svc;
-    
+    private AfcChargeSvc        svc;
+
     @ApiOperation("余额/返现金-充值")
     @PostMapping("/charge")
-    ChargeRo charge(@RequestBody ChargeTo to, final HttpServletRequest req) {
+    ChargeRo charge(@RequestBody final ChargeTo to, final HttpServletRequest req) throws NumberFormatException, ParseException {
         _log.info("余额/返现金-充值： {}", to);
         to.setUserId(to.getId());
         to.setId(null);
         to.setOrderId(RandomEx.randomUUID());
-        if(to.getTradeType() == ChargeTypeDic.BALANCECHARGE.getCode()) {
-        	to.setTradeTitle("余额充值");
-        }else if(to.getTradeType() == ChargeTypeDic.CASHBACKCHARGE.getCode()) {
-        	to.setTradeTitle("返现金充值");
+        if (to.getTradeType() == ChargeTypeDic.BALANCECHARGE.getCode()) {
+            to.setTradeTitle("余额充值");
+        } else if (to.getTradeType() == ChargeTypeDic.CASHBACKCHARGE.getCode()) {
+            to.setTradeTitle("返现金充值");
         }
-        
+
         if (!isDebug || to.getOpId() == null) {
-			Long userId = null;
-			String ip = null;
-			try {
-				userId = JwtUtils.getJwtUserIdInCookie(req);
-				ip = AgentUtils.getIpAddr(req, passProxy);
-			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			if (userId == null) {
-				ChargeRo ro = new ChargeRo();
-				ro.setResult(ChargeResultDic.NOT_FOUND_OP);
-				return ro;
-			}
-			to.setOpId(userId);
-			to.setIp(ip);
-		}
+            to.setOpId(JwtUtils.getJwtUserIdInCookie(req));
+            to.setIp(AgentUtils.getIpAddr(req, passProxy));
+        }
+        _log.debug("获取当前用户ID: {}", to.getOpId());
+
         to.setMac("不再获取MAC地址");
         return svc.charge(to);
     }
