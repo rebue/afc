68b5c902c81672d47a0acdca67e0a249e898539f
zbz <nnzbz@qq.com>
2018-11-23 20:53:38 +0800

    * 声明作废结算完成通知

--- afc-bll/src/main/java/rebue/afc/pub/SettleDonePub.java
+++ afc-bll/src/main/java/rebue/afc/pub/SettleDonePub.java
@@ -14,7 +14,10 @@ import rebue.sbs.rabbit.RabbitProducer;
 
 /**
  * 结算完成通知的发布者
+ * 
+ * @deprecated
  */
+@Deprecated
 @Component
 public class SettleDonePub implements ApplicationListener<ApplicationStartedEvent> {
     private static final Logger _log         = LoggerFactory.getLogger(SettleDonePub.class);
@@ -28,17 +31,18 @@ public class SettleDonePub implements ApplicationListener<ApplicationStartedEven
     private RabbitProducer      producer;
 
     @Override
-    public void onApplicationEvent(ApplicationStartedEvent event) {
+    public void onApplicationEvent(final ApplicationStartedEvent event) {
         // 防止多次启动
-        if (bStartedFlag)
+        if (bStartedFlag) {
             return;
+        }
         bStartedFlag = true;
 
         try {
-            _log.info("声明结算完成消息的Exchange: {}",AfcExchangeCo.SETTLE_DONE_EXCHANGE_NAME);
+            _log.info("声明结算完成消息的Exchange: {}", AfcExchangeCo.SETTLE_DONE_EXCHANGE_NAME);
             producer.declareExchange(AfcExchangeCo.SETTLE_DONE_EXCHANGE_NAME);
-        } catch (Exception e) {
-            String msg = "声明结算完成消息的Exchange失败";
+        } catch (final Exception e) {
+            final String msg = "声明结算完成消息的Exchange失败";
             _log.error(msg, e);
             throw new RuntimeException(msg, e);
         }
@@ -47,7 +51,7 @@ public class SettleDonePub implements ApplicationListener<ApplicationStartedEven
     /**
      * 发送结算完成的消息
      */
-    public void send(SettleDoneMsg msg) {
+    public void send(final SettleDoneMsg msg) {
         _log.info("发送结算完成的消息");
         producer.send(AfcExchangeCo.SETTLE_DONE_EXCHANGE_NAME, msg);
     }
